4f18c5eba8eda914a9a30268e2940a6d6dcf60c4
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-11-16 12:28:09 +0100

    Require vendor autoloader in class definition
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -69,6 +69,8 @@ use OCP\User\Events\UserDeletedEvent;
 use OCP\Util;
 use Psr\Container\ContainerInterface;
 
+include_once __DIR__ . '/../../vendor/autoload.php';
+
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'mail';
 
@@ -77,10 +79,6 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function register(IRegistrationContext $context): void {
-		if ((@include_once __DIR__ . '/../../vendor/autoload.php') === false) {
-			throw new Exception('Cannot include autoload. Did you run install dependencies using composer?');
-		}
-
 		$context->registerParameter('hostname', Util::getServerHostName());
 
 		$context->registerService('userFolder', function (ContainerInterface $c) {
