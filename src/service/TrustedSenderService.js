32fed103e71626815482d063d9e9ebd3da36553c
GretaD <gretadoci@gmail.com>
2020-11-24 17:28:28 +0100

    Front end
    Signed-off-by: GretaD <gretadoci@gmail.com>

---
+++ src/service/TrustedSenderService.js
@@ -0,0 +1,16 @@
+import { generateUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+
+
+export async function trustSender(email, trustFlag) {
+	const url = generateUrl('/apps/mail/api/trustedsenders/{email}', {
+		email,
+	})
+
+	 if ( trustFlag) {
+		 await	axios.put(url)
+	}
+	else {
+		 await	axios.delete(url)
+	}
+}
